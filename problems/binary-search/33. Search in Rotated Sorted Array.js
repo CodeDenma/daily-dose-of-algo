05beea982404badaeb2112167010edb420617374
Henry Park <codedenma@gmail.com>
2023-02-19 18:38:18 -0800

    search in rotated sorted array complexity analysis

--- problems/binary-search/33. Search in Rotated Sorted Array.js
+++ problems/binary-search/33. Search in Rotated Sorted Array.js
@@ -1,30 +1,9 @@
-// class Solution:
-//     def search(self, nums: List[int], target: int) -> int:
-//         l, r = 0, len(nums) - 1
-
-//         while l <= r:
-//             m = (l + r) // 2
-
-//             if nums[m] == target:
-//                 return m
-//             elif nums[l] <= nums[m]:
-//                 if nums[l] <= target < nums[m]:
-//                     r = m - 1
-//                 else:
-//                     l = m + 1
-//             else:
-//                 if nums[m] < target <= nums[r]:
-//                     l = m + 1
-//                 else:
-//                     r = m - 1
-
-//         return -1
-
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number}
  */
+// O(log(n)) Time | O(1) Space
 var search = function (nums, target) {
   let left = 0,
     right = nums.length - 1;
