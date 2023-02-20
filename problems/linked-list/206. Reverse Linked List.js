07510fa7a2eaf7a863244802783f6704e7db2d7f
Henry Park <codedenma@gmail.com>
2023-02-20 14:16:47 -0800

    reverse linked list

---
+++ problems/linked-list/206. Reverse Linked List.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+// O(n) Time | O(1) Space
+var reverseList = function (head) {
+  let prevNode = null,
+    node = head;
+  let nextNode = null;
+
+  while (node) {
+    nextNode = node.next;
+    node.next = prevNode;
+    prevNode = node;
+
+    node = nextNode;
+  }
+
+  return prevNode;
+};
