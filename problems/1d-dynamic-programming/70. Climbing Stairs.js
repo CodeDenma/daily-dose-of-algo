ed42d8f087606d639262eafc6792b1d256067815
codedenma <codedenma@gmail.com>
2023-04-05 23:17:56 -0700

    climbing stairs

---
+++ problems/1d-dynamic-programming/70. Climbing Stairs.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function (n) {
+  if (n <= 2) return n;
+  let oneStepAgo = 1,
+    twoStepsAgo = 1;
+
+  for (let i = 2; i < n; i++) {
+    const temp = oneStepAgo;
+    oneStepAgo += twoStepsAgo;
+    twoStepsAgo = temp;
+  }
+
+  return oneStepAgo + twoStepsAgo;
+};
+
+console.log(climbStairs(2)); // 2
+console.log(climbStairs(3)); // 3
