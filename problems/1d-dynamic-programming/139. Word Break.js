a5fda6d62a7b37312f7019a69d4b6bd9f67aefcc
Henry Park <codedenma@gmail.com>
2023-04-14 21:29:00 -0700

    word break

---
+++ problems/1d-dynamic-programming/139. Word Break.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
+// neetcode solution
+var wordBreak = function(s, wordDict) {
+  // true means that a we can generate all the characters starting from the given index with words from the wordDict
+  const table = new Array(s.length + 1).fill(false);
+  // since we don't need any word to generate an empty string (string sliced from the last index to the end of the string is empty)
+  table[table.length - 1] = true
+
+  for (let i = table.length - 1; i >= 0; i--) {
+    for (const word of wordDict) {
+      if (i + word.length <= s.length && s.slice(i, i + word.length) === word) {
+        table[i] = table[i + word.length]
+
+        if (table[i]) break;
+      }
+    }
+  }
+
+  return table[0]
+};
