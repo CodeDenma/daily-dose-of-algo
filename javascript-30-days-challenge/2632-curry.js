83ada0b9d7eb5b23e120594b4d5173991d85c134
Henry Park <codedenma@gmail.com>
2023-05-14 17:29:59 -0700

    js30 curry

---
+++ javascript-30-days-challenge/2632-curry.js
@@ -0,0 +1,26 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var curry = function(fn) {
+  return function curried(...args) {
+    if (args.length < fn.length) {
+      return function(...args2) {
+        return curried(...args, ...args2);
+      }
+    }
+
+    return fn(...args);
+  }
+};
+
+/**
+ * function sum(a, b) { return a + b; }
+ * const csum = curry(sum);
+ * csum(1)(2) // 3
+ */
+
+const sum = (a, b) => a + b;
+const csum = curry(sum);
+
+console.log(csum(1)(2));
