58612a8efbf71ac996aa5e6395b8397e706c8c1a
Henry Park <codedenma@gmail.com>
2023-05-25 12:18:35 -0700

    scratch

--- scratch.js
+++ scratch.js
@@ -1,43 +1,4 @@
 
 
 
-/**
- * @param {string[]} strs
- * @return {string[][]}
- */
-var groupAnagrams = function(strs) {
-  const wordBank = {};
 
-  for (const word of strs) {
-    const sortedWord = word.split('').sort();
-
-    wordBank[sortedWord] = (wordBank[sortedWord] ?? []).concat(word);
-  }
-
-
-  return Object.values(wordBank);
-};
-
-console.log(groupAnagrams(["eat","tea","tan","ate","nat","bat"]));
-
-var groupAnagrams = function(strs) {
-  const wordBank = {};
-  const aCharCode = "a".charCodeAt();
-  
-  for (const word of strs) {
-    const alphabets = new Array(26).fill(0);
-    
-    
-    for (const char of word) {
-      const index = char.charCodeAt() - aCharCode;
-
-      alphabets[index]++;
-    }
-
-    const key = JSON.stringify(alphabets);
-
-    wordBank[key] = (wordBank[key] ?? []).concat(word);
-  }
-  
-  return Object.values(wordBank);
-}
